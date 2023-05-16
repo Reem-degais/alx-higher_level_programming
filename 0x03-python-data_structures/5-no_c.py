e66d1de10f1a40f58322a62c4788a89390657b5f
Reem-degais <reem.degais.33@gmail.com>
2023-05-15 17:23:29 -0700

    -

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    list_of_str = list(my_string)
-    for i in list_of_str:
-        if i == "C" or i == "c":
-            list_of_str.remove(i)
-    return("".join(list_of_str))
+    new_string = ""
+    for c in my_string:
+        if c not in "Cc":
+            new_string += c
+    return new_string
