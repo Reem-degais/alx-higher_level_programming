40f979da331fd983baffb2eb653907e5d441465b
Reem-degais <reem.degais.33@gmail.com>
2023-05-17 17:49:14 -0700

    -

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    a=sorted(a_dictionary)
+    for x in a :
+        for y in a_dictionary:
+            if x == y:
+                print ("{}: {}".format(y, a_dictionary[y]))
