4f696d5d2a831cef749e1c0ae31e136152313761
Reem Degais <reem.degais.33@gmail.com>
2023-05-29 09:27:01 -0700

    -

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    i = 0
     nb_print = 0
 
-    for i in range(0, x):
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             nb_print += 1
-        except():
-            continue
-    print()
+        except IndexError:
+            break
+    print("")
     return nb_print
