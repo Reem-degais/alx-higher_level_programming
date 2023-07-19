1a19e666f97f80221ec386a0809d82efcf16cd52
Reem-degais <reem.degais.33@gmail.com>
2023-07-18 19:34:37 -0700

    -

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
--- lists all records of second table where name is not NULL ordered by score
+-- lists all  records of second table where name is not NULL ordered by score
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL
