d7fd79bf20d2cd71f77a38b6e498e647b2056c9d
Reem-degais <reem.degais.33@gmail.com>
2023-08-20 18:13:31 -0700

    -

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE \
-                BINARY `name` = '{}'".format(sys.argv[4]))
+                BINARY `name` = '{}'".format(argv[4]))
     rows = cur.fetchall()
     for i in rows:
         print(i)
