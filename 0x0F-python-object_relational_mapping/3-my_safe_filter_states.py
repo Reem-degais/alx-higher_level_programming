203d945901db8991b1d61ddfe5f241d9296fe93f
Reem-degais <reem.degais.33@gmail.com>
2023-08-25 18:55:16 -0700

    -

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,8 +11,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE \
-                BINARY `name` = '{}'".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
     rows = cur.fetchall()
     for i in rows:
         print(i)
