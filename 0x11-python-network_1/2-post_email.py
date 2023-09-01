5c5a0b775f5cb5543d2ef75c2a309767879945c5
Reem-degais <reem.degais.33@gmail.com>
2023-09-01 11:25:45 -0700

    -

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,16 +4,16 @@ with the email as a parameter,
 and displays the body of the response (decoded in utf-8)"""
 
 import urllib.request
-import sys
 import urllib.parse
-
+import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    values = {'emial': sys.argv[2]}
 
-    data = urllib.parse.urlencode(values).encode()
+    url = sys.argv[1]
+    new_value = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(new_value).encode()
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        r = response.read().decode('utf8')
-    print("Your email is: {}".format(r))
+        val = response.read().decode('utf8')
+    print(val)
