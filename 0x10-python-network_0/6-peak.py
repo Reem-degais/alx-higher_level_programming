7a6fa33d27689d51b615abdba8405959aba136d4
Reem-degais <reem.degais.33@gmail.com>
2023-08-31 18:13:47 -0700

    fint peak value

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" finds a peak in a list of unsorted integers"""
+def find_peak(list_of_integers):
+    max_int = None
+    if list_of_integers is None:
+        return None
+    else:
+        for i in list_of_integers:
+            if max_int is None or max_int < i:
+                max_int = i
+        return max_int
