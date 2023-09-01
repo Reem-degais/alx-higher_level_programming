c7e0da49f02922d863658f230782bcb51c6b2afa
Reem-degais <reem.degais.33@gmail.com>
2023-09-01 14:13:42 -0700

    -

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8)"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    req = Request(argv[1])
+    try:
+        response = urlopen(req)
+    except HTTPError as e:
+        print('Error code: ', e.code)
