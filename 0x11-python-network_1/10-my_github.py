761f955e0fcd300cd31b1e5f2f1987bb3f94a31c
Reem-degais <reem.degais.33@gmail.com>
2023-09-01 17:25:37 -0700

    -

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,8 +9,4 @@ import sys
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/user',
                      auth=(sys.argv[1], sys.argv[2]))
-    json = r.json()
-    try:
-        print(json['id'])
-    except:
-        print("None")
+    print(r.json().get('id'))
